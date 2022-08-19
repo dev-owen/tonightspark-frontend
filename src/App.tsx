9af56cca365309e8e4796c089eff0fc2ac0bbd0f
dev-owen <jgyuity1289@gmail.com>
2022-08-20 01:35:32 +0900

    font 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="font-bold">font weight 800 입니다.</p>
+      <p className="font-medium">font weight 600 입니다.</p>
+      <p className="font-light">font weight 400 입니다.</p>
     </div>
   );
 }
