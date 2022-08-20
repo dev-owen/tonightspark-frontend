2c35b0b35da778458ae444f07ef81735bd28b31f
inwalter99 <inwalter99@gmail.com>
2022-08-20 19:39:24 +0900

    사이드바 스타일링

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
 import * as $ from './style';
 import { useRecoilState } from 'recoil';
 import { creatorHashState } from '../../state/creatorHashState';
-import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router';
+import Logo from '../Logo';
+import Divider from '../Divider';
 
 const Sidebar = () => {
+  const location = useLocation()
   const [hash, setHash] = useRecoilState(creatorHashState);
-  const navigate = useNavigate();
+  const zepLink = `https://zep.us/play/${hash}`
 
   return (
     <$.Wrapper>
-      <p onClick={() => navigate('/home')}>Participant Insight</p>
-      <p onClick={() => navigate('/home/hour')}>Participation by Hour</p>
-      <p onClick={() => navigate('/home/area')}>Area Information</p>
-      <p onClick={() => navigate('/home/object')}>Object Insight</p>
-      <p className="bottomSection">ZEP 접속링크 : https://zep.us/play/{hash}</p>
+      <$.Top>
+        <$.LogoWrapper>
+          <Logo />
+        </$.LogoWrapper>
+        <Divider width="220px" height="1px" margin="40px 0 32px 0" />
+        <$.MenuLinkList>
+          <$.MenuLink to={'/home'} current={location.pathname === '/home'}>Participant Insight</$.MenuLink>
+          <$.MenuLink to={'/home/hour'} current={location.pathname === '/home/hour'}>Participation by Hour</$.MenuLink>
+          <$.MenuLink to={'/home/area'} current={location.pathname === '/home/area'}>Area Information</$.MenuLink>
+        </$.MenuLinkList>
+      </$.Top>
+      <$.Bottom>
+        <a href={zepLink}>
+          <p>ZEP 접속링크 :</p>
+          <p>{zepLink}</p>
+        </a>
+        <Divider width="220px" height="1px" margin="28px 0 28px 0" />
+        <p className='copyright'>All rights reserved ⓒ TonightSpark</p>
+      </$.Bottom>
     </$.Wrapper>
   );
 };
--- src/components/Sidebar/style.ts
+++ src/components/Sidebar/style.ts
@@ -1,5 +1,6 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { GRAY_500, WHITE } from '../../utils/color';
+import { GRAY_200, GRAY_500, INDIGO_10, PURPLE_100, WHITE } from '../../utils/color';
 
 export const Wrapper = styled.div`
   width: 300px;
@@ -10,8 +11,54 @@ export const Wrapper = styled.div`
   color: ${GRAY_500};
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
 
-  .bottomSection {
-    margin-top: auto;
+export const Top = styled.div`
+  padding-top: 60px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Bottom = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 12px;
+  font-weight: normal;
+  padding-bottom: 40px;
+  text-align: left;
+  p {
+    margin: 0;
   }
+  a > p {
+    margin-top: 8px;
+  }
+  .copyright {
+    color: ${GRAY_200};
+  }
+`;
+
+export const LogoWrapper = styled.span`
+  margin-left: 20px;
+  width: fit-content;
+`
+export const MenuLinkList = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  flex-direction: column;
+  flex-wrap: wrap;
+  gap: 16px;
+`;
+
+export const MenuLink = styled(Link)<{ current: boolean }>`
+  background-color: ${({ current }) => (current ? INDIGO_10 : 'none')};
+  color: ${({ current }) => (current ? PURPLE_100 : GRAY_500)};
+  padding: 0 20px;
+  height: 48px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 24px;
 `;
