1fe0088277b65bd77a3595cdb93b4dbad136763a
inwalter99 <inwalter99@gmail.com>
2022-08-21 09:03:31 +0900

    불필요한 로딩 제거

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,38 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { useRecoilState } from 'recoil';
-import { creatorHashState } from '../../state/creatorHashState';
 import * as $ from './style';
-import Snippet from '../Snippet';
 import Sidebar from '../Sidebar';
 import { Route, Routes } from 'react-router-dom';
 import ParticipationHour from '../ParticipationHour';
 import AreaInformationPage from '../AreaInformationPage';
 import ParticipantInsightPage from '../ParticipantInsightPage';
-import { useLocation } from 'react-router';
 
 const HomePage = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const location = useLocation();
-  useEffect(() => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-  }, [location.pathname]);
-
   return (
     <$.Wrapper>
       <$.HomeContainer>
         <Sidebar />
-        {isLoading ? (
-          <Snippet />
-        ) : (
           <Routes>
             <Route index element={<ParticipantInsightPage />} />
             <Route path="hour" element={<ParticipationHour />} />
             <Route path="area" element={<AreaInformationPage />} />
           </Routes>
-        )}
       </$.HomeContainer>
     </$.Wrapper>
   );
